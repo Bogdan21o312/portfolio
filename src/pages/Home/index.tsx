1ec12c690d494f9966aff80c1d7c587915b2e9e3
Bogdan21o312 <lutskiv2010@gmail.com>
2022-11-19 09:12:49 +0200

    Updated

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import classes from "./Home.module.scss"
 import Layout from "../../components/@UI/Layout";
+import {Text} from "../../components/@UI/Text";
 
 const Index = () => {
 
     return (
         <Layout>
-
+            <Text size={'small'} color={'pink'}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium facere laudantium modi? Amet, atque dolorem est eveniet ex impedit in inventore, libero magni maiores minima nemo nesciunt nulla, officia voluptas.
+            </Text>
         </Layout>
     );
 };
