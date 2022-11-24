08eef750b025fcd066e4210afd2e52b3ed62476c
Bogdan21o312 <lutskiv2010@gmail.com>
2022-11-24 14:38:39 +0200

    Updated

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,6 +7,12 @@ const Index = () => {
 
     return (
         <Layout>
+            <Text size={'big'} color={'pink'}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium facere laudantium modi? Amet, atque dolorem est eveniet ex impedit in inventore, libero magni maiores minima nemo nesciunt nulla, officia voluptas.
+            </Text>
+            <Text size={'medium'} color={'pink'}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium facere laudantium modi? Amet, atque dolorem est eveniet ex impedit in inventore, libero magni maiores minima nemo nesciunt nulla, officia voluptas.
+            </Text>
             <Text size={'small'} color={'pink'}>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium facere laudantium modi? Amet, atque dolorem est eveniet ex impedit in inventore, libero magni maiores minima nemo nesciunt nulla, officia voluptas.
             </Text>
